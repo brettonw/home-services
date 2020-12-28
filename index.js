fe3954eadc65365a469ba9ecda2defc33c93ec42
Bretton Wade <bretton.wade@gmail.com>
2020-12-28 16:49:59 -0500

    testing

--- index.js
+++ index.js
@@ -124,8 +124,8 @@ let refresh = function () {
                     .add ("http://www.w3.org/2000/svg;circle", { attributes: { cx: 0, cy: 0, r: radius * 0.8, stroke: "#444", "stroke-width": 0.025, fill: "white" } })
                     .add ("http://www.w3.org/2000/svg;text", { attributes: { x: right, y: 0, fill: "black", "font-family": "Helvetica, Arial", "font-size": 0.65, "text-anchor": "end", "dominant-baseline": "middle" }, innerHTML: v1 })
                     .add ("http://www.w3.org/2000/svg;text", { attributes: { x: right + v2CharSpacing, y: 0, fill: "black", "font-family": "Helvetica, Arial", "font-size": 0.4, "text-anchor": "start", "dominant-baseline": "middle" }, innerHTML: v2 })
-            .add ("http://www.w3.org/2000/svg;text", { attributes: { x: 0, y: 0.5, fill: "purple", "font-family": "Helvetica, Arial", "font-size": 0.65, "text-anchor": "start", "dominant-baseline": "middle" }, innerHTML: "0000000000" })
-            .add ("http://www.w3.org/2000/svg;text", { attributes: { x: 0, y: 0.75, fill: "green", "font-family": "Helvetica, Arial", "font-size": 0.4, "text-anchor": "start", "dominant-baseline": "middle" }, innerHTML: "0000000000" })
+            .add ("http://www.w3.org/2000/svg;text", { attributes: { x: 0, y: 0, dy: "50%", fill: "purple", "font-family": "Helvetica, Arial", "font-size": 0.65, "text-anchor": "start" }, innerHTML: "0000000000" })
+            .add ("http://www.w3.org/2000/svg;text", { attributes: { x: 0, y: 0, dy: "50%", fill: "green", "font-family": "Helvetica, Arial", "font-size": 0.4, "text-anchor": "start" }, innerHTML: "0000000000" })
                 .end ()
             .end ();
     };
