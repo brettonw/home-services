37a27d0f02ed1ee2aa9b1e8cbd82858d18c30d62
Bretton Wade <bretton.wade@gmail.com>
2020-12-30 15:25:15 -0500

    update to use the last hop in comcast as the common ISP monitor

--- bin/run.sh
+++ bin/run.sh
@@ -5,6 +5,6 @@ if [ ! -d "$targetDir/out" ]; then
   mkdir -p "$targetDir/out";
 fi
 nohup $targetDir/bin/get-temperature.sh > $targetDir/out/get-temperature.sh.out 2>&1 &
-nohup $targetDir/bin/get-ping.sh 1.1.1.1 > $targetDir/out/get-ping-1.1.1.1.out 2>&1 &
+nohup $targetDir/bin/get-ping.sh 68.87.168.53 > $targetDir/out/get-ping-68.87.168.53.out 2>&1 &
 nohup $targetDir/bin/get-ping.sh 96.120.104.221 > $targetDir/out/get-ping-96.120.104.221.out 2>&1 &
 ps -ef | grep "get-" | grep -v "grep"
