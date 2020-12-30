a3d73ecfe24bf774ff47d7285403cac6897e8e5e
Bretton Wade <bretton.wade@gmail.com>
2020-12-30 15:16:14 -0500

    monitor traceroute update to suss out the last common ip address in our ISP

--- bin/monitor-traceroute.pl
+++ bin/monitor-traceroute.pl
@@ -31,29 +31,78 @@ sub gatherRouteHost {
         print STDERR "$index $ip [$line]\n";
         $expectedIndex++;
 
+        # vivify the hash reference
+        if (! exists ($routeHosts->[$index])) {
+            $routeHosts->[$index] = {};
+        }
+        my $hashRef = $routeHosts->[$index];
 
+        # increment the count of the number of times we've seen this ip
+        $hashRef->{$ip} = exists ($hashRef->{$ip}) ? ($hashRef->{$ip} + 1) : 0;
     } else {
-        print STDERR "UNEXPECTED INDEX ($index) WHEN EXPECTING ($expectedIndex). [$line]\n";
+        #print STDERR "UNEXPECTED INDEX ($index) WHEN EXPECTING ($expectedIndex). [$line]\n";
     }
     return $expectedIndex;
+}
+
+sub getRouteHostCount {
+    my ($index) = @_;
+    my $hashRef = $routeHosts->[$index];
+    my $routeHost = (keys (%$hashRef))[0];
+    return $hashRef->{$routeHost};
+}
+
+sub scrubRouteHosts {
+    # adjust the maxHops value. start by getting the expected count from the first route host, which is our router
+    my $expectedCount = getRouteHostCount (1);
+    for (my $i = 2; $i < scalar (@$routeHosts); $i++) {
+        if (exists ($routeHosts->[$i])) {
+            # print everything in this reference
+            my $hashRef = $routeHosts->[$i];
+
+            if (scalar(%$hashRef) == 1) {
+                my $routeHost = (keys (%$hashRef))[0];
+                my $routeHostCount = $hashRef->{$routeHost};
+                if ($routeHostCount == $expectedCount) {
+                    print STDERR "$i $routeHost\n";
+                    $maxHops = $i;
+                } else {
+                    #print STDERR "RouteHousts at index ($i) has exactly one host, but does not match the expected count ($routeHostCount -> $expectedCount).\n";
+                    last;
+                }
+            } else {
+                #print STDERR "RouteHousts at index ($i) doesn't have exactly one host\n";
+                last;
+            }
+        } else {
+            #print STDERR "Empty RouteHousts at index ($i)\n";
+            last;
+        }
+    }
+    print STDERR "\nAdjusting maxHops to $maxHops\n\n";
 }
 
 while (1) {
     for my $targetHost (@$targetHosts) {
         # traceroute gives output for lines we care about will have the form:
         # " 3  24.124.180.217  9.795 ms  9.270 ms  10.184 ms"
+        print STDERR "TARGET: $targetHost\n";
         my $expectedIndex = 1;
-        for my $line (split (/^/, `traceroute -n -m $maxHops -z 0.1 $targetHost`)) {
+        for my $line (split (/^/, `traceroute -n -m $maxHops -z 50 $targetHost 2>&1`)) {
             chomp $line;
             if ($line =~ /^\s+(\d)\s+(\d+\.\d+\.\d+\.\d+)(\s+(\d+\.\d+\s+ms|\*)){3}$/) {
                 $expectedIndex = gatherRouteHost ($line, $expectedIndex, $1, $2);
             } elsif ($line =~ /^\s+(\d)\s+(\*\s+)+(\d+\.\d+\.\d+\.\d+)(\s+(\d+\.\d+\s+ms|\*)){2,}$/) {
                 $expectedIndex = gatherRouteHost ($line, $expectedIndex, $1, $3);
             } else {
-                print STDERR ("BAD LINE. [$line]\n");
+                #print STDERR ("BAD LINE. [$line]\n");
             }
         }
         print STDERR "\n";
     }
-    exit ();
+
+    # adjust the maxHops value, start by getting the expected count from the original host, which is certainly our router
+    scrubRouteHosts ();
+
+    sleep (20);
 }
