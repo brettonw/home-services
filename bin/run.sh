f9d02589a37772b3da0ce706706329222fb13329
Bretton Wade <bretton.wade@gmail.com>
2021-01-14 17:22:44 -0500

    remove extransour code/comments

--- bin/run.sh
+++ bin/run.sh
@@ -28,7 +28,3 @@ function restartProc () {
 restartProc "get-temperature.sh";
 restartProc "get-ping.sh 68.87.168.53";
 restartProc "get-ping.sh 96.120.104.221";
-
-#nohup $targetDir/bin/get-ping.sh 68.87.168.53 > $targetDir/out/get-ping-68.87.168.53.out 2>&1 &
-#nohup $targetDir/bin/get-ping.sh 96.120.104.221 > $targetDir/out/get-ping-96.120.104.221.out 2>&1 &
-#ps -ef | grep "get-" | grep -v "grep"
