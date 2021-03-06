001c10a620476e5a635fb2151b66a9b7cd5895da
Bretton Wade <bretton.wade@gmail.com>
2021-03-06 00:27:05 -0500

    main - actually remove for reinstall

--- integrations/bin/reinstall.bash
+++ integrations/bin/reinstall.bash
@@ -5,6 +5,8 @@ workingPath="/home/brettonw/bin";
 sensorFile="$workingPath/sensor.py";
 if [ -e "$sensorFile" ]; then
   echo "Removing existing sensor configuration";
+  ls -l "$sensorFile";
+  rm -f "$sensorFile";
 fi
 
 . /home/brettonw/bin/install.bash;
